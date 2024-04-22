d2658d9818544240b4614bf087494f571fa6f8db
Karen Beate <karen.beate@outlook.com>
2024-04-22 12:50:10 +0200

    middleware

---
+++ services/middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+//UserRoleId = 1(Admin), UserName.
+
+module.exports = {
+
+  //anyone registered, user and admin
+  isUser : (req, res, next) => {
+    !req.headers.authorization?.split(' ')[1] ? 
+      res.status(401).json({ status : "error", statusCode : 401, data : { result : "Unauthorized for unregistered users, please register as customer to continue." }}) :
+        jwt.verify( req.headers.authorization.split(' ')[1], process.env.TOKEN_SECRET, ( err,valid ) => {
+          if( err ){ res.status(401).json({ status : "error", statusCode : 401, data : { result : err.message } }) }
+          else{ next(); }
+        })
+  },
+
+  //just Admin(userRole = 1)
+  isAdmin : ( req, res, next ) => {
+  !req.headers.authorization?.split(' ')[1] ?   
+    res.status(401).json({ status : "error", statusCode : 401, data : { result : "Unauthorized." }}) :
+      jwt.verify(req.headers.authorization.split(' ')[1], process.env.TOKEN_SECRET, ( err,valid ) => {
+        if(err){res.status(401).json({ status : "error", statusCode : 401, data : { result : err.message } }) }
+          if( valid.UserRoleId == 1 ){ next(); }
+          else{ res.status(401).json({ status : "error", statusCode : 401, data : { result : "Unauthorized."} }) }
+      })
+    }
+};
