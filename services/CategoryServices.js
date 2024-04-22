22964e40ac96e6698cd67e654a4c746434371c69
Karen Beate <karen.beate@outlook.com>
2024-04-22 19:36:50 +0200

    categories

--- services/CategoryServices.js
+++ services/CategoryServices.js
@@ -4,11 +4,30 @@ class CategoryService {
         this.Category = db.Category;
     }
 
+    async getCategory(categoryid){
+        return this.Category.findOne({
+            where : { id: categoryid }
+        }).catch( err => { console.log(err); return err }) 
+    }    
+
+    async getCategories(){
+        return await this.Category.findAll()
+        .catch( err => { console.log(err); return err })
+    }    
+
     async createCategory(Category){
         return await this.Category.create({
             Category : Category
         })
-        .catch(err => {console.log(err); return err })
+        .catch( err => { console.log(err); return err})
+    }
+
+        //only alter Categoryname of id.
+    async alterCategory(Categoryid, newCategory){
+        return await this.Category.update({
+            Category : newCategory
+        }, { where : { id: Categoryid }})
+        .catch( err => { console.log(err); return err })
     }
 
     //no delete if in use, restrict.
@@ -16,26 +35,7 @@ class CategoryService {
         return await this.Category.destroy({
             where : { id : Categoryid}
         })
-        .catch(err => {console.log(err); return err })
-    }
-
-    async getCategories(){
-        return await this.Category.findAll()
-        .catch(err => {console.log(err); return err })
-    }
-
-    async getCategory(category){
-        return this.Category.findOne({
-            where : { Category : category }
-        }).catch(err => {console.log(err); return err }) 
-    }
-
-        //only alter Categoryname of id.
-    async alterCategory(oldCategoryid, newCategory){
-        return await this.Category.update({
-            Category : newCategory
-        }, { where : { id: oldCategoryid }})
-        .catch(err => {console.log(err); return err })
+        .catch( err => { console.log(err); return err })
     }
 
 }
