6430cf1a3e869fbc53805cfd636a200dfd7bff62
Karen Beate <karen.beate@outlook.com>
2024-04-22 19:37:46 +0200

    categories app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const initRouter = require('./routes/init');
 const authRouter = require('./routes/auth');
+const categoriesRouter = require('./routes/categories');
 
 var db = require("./models");
 db.sequelize.sync({ force : false });
@@ -26,11 +26,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/init', initRouter )
 app.use('/auth', authRouter);
+app.use('/categories', categoriesRouter);
 
-// catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -45,5 +45,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+  
 module.exports = app;
