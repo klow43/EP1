86bc5fd8bb734fbedd20b6504e9fad11afd359d9
Karen Beate <karen.beate@outlook.com>
2024-05-01 20:08:08 +0200

    user id in jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ router.post('/login', async function(req, res, next) {
     }
     if(userdb !== null && await bcrypt.compare(user.password, userdb.password.toString()) == true){
         try{
-            token = jwt.sign({ User : userdb.email , UserRoleId : userdb.UserRole.dataValues.RoleId }, process.env.TOKEN_SECRET, { expiresIn : '2h' })
+            token = jwt.sign({ User : userdb.email , UserRoleId : userdb.UserRole.dataValues.RoleId, UserId : userdb.id }, process.env.TOKEN_SECRET, { expiresIn : '2h' })
         }catch(err){console.log(err); res.status(500).json({ status : "error", statusCode : 500, data : { result : "Unable to login."}})}  
     res.status(200).json({ status : "success", statusCode : 200, data : { result : "You are logged in",id : userdb.id, email : userdb.email, userName : userdb.userName, token : token}})     
     }
