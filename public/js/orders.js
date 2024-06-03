68ce15fadbe6e9f807f5b26642ac17bc4dd75e11
Karen Beate <karen.beate@outlook.com>
2024-06-03 19:03:23 +0200

    /admin/orders delete

--- public/js/orders.js
+++ public/js/orders.js
@@ -5,10 +5,23 @@ modal.addEventListener('show.bs.modal', function (event) {
     if(info != null){
         obj = JSON.parse(info)
         document.getElementById('modalPut').innerHTML = '<input id="orderid" class="form-control" name="orderid" readonly>'
-        document.getElementById('orderid').value = obj.OrderId;
+        document.getElementById('orderid').value = obj.orderId;
     }
     else{     
         document.getElementById('id').value = "",
         document.getElementById('modalPut').innerHTML = ''; 
     }   
-})
+}) 
+
+async function deleteOrder(order){
+    await fetch('http://localhost:3000/admin/orders', {
+        method : 'DELETE',
+        headers : { 'Content-Type': 'application/json'},
+        body : JSON.stringify({ orderid :  order.orderId })
+    }).then((res) => {
+        if( res.ok ) {
+            location.reload();
+            return Promise.resolve(res);
+        }
+    }).catch((err) => { return Promise.reject( err )})
+} 
